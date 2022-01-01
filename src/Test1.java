95110ae7c1dc502b422c02722548470aa5653321
pranjalikharat111 <pranjalikharat111@gmail.com>
2022-01-01 20:38:57 +0530

    exe modification

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "Yes";
+		if(hungry=="yes") {
+			System.out.println("plz eat something");
+		}else {
+			System.out.println("do something");
+		}
+
+			}
+
+	}
+
+
